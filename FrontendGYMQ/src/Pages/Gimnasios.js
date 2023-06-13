762f4cc7b4c4ab67f39648bbd1c2ba899cc11ac5
Ana Sofia Ricardo Aguilar <anasofiaricardoaguilar@gmail.com>
2023-06-12 19:24:10 -0500

    12/06/2023  7:24

--- FrontendGYMQ/src/Pages/Gimnasios.js
+++ FrontendGYMQ/src/Pages/Gimnasios.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { NavBar } from '../Layouts/NavBar/NavBar';
 import '../App.css'
+import Information from "../Ui/Cards/InformationCards";
 
 export function Gimnasios() {
     return (
-        <div>
+        <div className="containerGymInformation">
             <div className='header'><NavBar/></div>
-            <div className="mainGym"></div>
+            <div className="mainGym"><Information/></div>
         </div>
     )
 }
