e9a79614e4f9ea707da644780a2eb9e3a764da3b
Ana Sofia Ricardo Aguilar <anasofiaricardoaguilar@gmail.com>
2023-06-13 21:41:13 -0500

    13/06/2023  9:41

--- FrontendGYMQ/src/Layouts/DataStore/DataStore.js
+++ FrontendGYMQ/src/Layouts/DataStore/DataStore.js
@@ -5,7 +5,9 @@ const DataStore = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [searchQuery, setSearchQuery] = useState('');
-  const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState([]);   
+  const [showCartModal, setShowCartModal] = useState(false);
+
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
@@ -28,6 +30,14 @@ const DataStore = () => {
   const removeFromCart = (item) => {
     const updatedCart = cartItems.filter((cartItem) => cartItem.id !== item.id);
     setCartItems(updatedCart);
+  };
+
+  const openCartModal = () => {
+    setShowCartModal(true);
+  };
+
+  const closeCartModal = () => {
+    setShowCartModal(false);
   };
 
   const cards = [
@@ -210,6 +220,7 @@ const DataStore = () => {
       category: 'Categoría 1',
       description: 'Esta es la descripción de la Tarjeta 20',
       price: '000',
+      mandarCarrito: '',
       image: 'https://res.cloudinary.com/duxhiuugx/image/upload/v1686584437/resilience-cbd-J9yRqbAuPTU-unsplash_eepjhw.jpg',
     },
 
@@ -391,6 +402,7 @@ const DataStore = () => {
       category: 'Categoría 2',
       description: 'Esta es la descripción de la Tarjeta 20',
       price: '000',
+      mandarCarrito: '',
       image: 'https://res.cloudinary.com/duxhiuugx/image/upload/v1686584437/resilience-cbd-J9yRqbAuPTU-unsplash_eepjhw.jpg',
     },
 
@@ -572,6 +584,7 @@ const DataStore = () => {
       category: 'Categoría 3',
       description: 'Esta es la descripción de la Tarjeta 20',
       price: '000',
+      mandarCarrito: '',
       image: 'https://res.cloudinary.com/duxhiuugx/image/upload/v1686584437/resilience-cbd-J9yRqbAuPTU-unsplash_eepjhw.jpg',
     },
   ];
@@ -601,7 +614,36 @@ const DataStore = () => {
                 />
                 </div>
             )}
-        </div>
+            <button className='buttonCart' onClick={openCartModal}>
+              <svg className='iconCart' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart2" viewBox="0 0 16 16">
+                <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+              </svg>
+            </button> 
+            {showCartModal && (
+              <div className='cartModal'>
+                <div className='cartModalContent'>
+                  <h3>Carrito de Compras</h3>
+                  {cartItems.length > 0 ? (
+                  <ul className='contentModal'>
+                    {cartItems.map((card) => (
+                      <StoreCards
+                        key={card.id}
+                        title={card.title}
+                        description={card.description}
+                        price={card.price}
+                        image={card.image}
+                        mandarCarrito={() => removeFromCart(card)}
+                      />
+                    ))}
+                  </ul>
+                ) : (
+                  <p>El carrito está vacío.</p>
+                )}
+                <button className='buttonCart' onClick={closeCartModal}>Cerrar</button>
+                </div>
+              </div>
+            )}
+      </div>
       {isOpen && (
         <ul className="categorys">
           <li onClick={() => handleCategorySelect('Categoría 1')}>Ropa</li>
@@ -620,9 +662,8 @@ const DataStore = () => {
                   description={card.description}
                   price={card.price}
                   image={card.image}
-                  mandarCarrito={addToCart(card)}
+                  mandarCarrito={() => addToCart(card)}
                 />
-                  // <button onClick={() => addToCart(card)}>Agregar al carrito</button>
               ))
             ) : (
               <p>No hay tarjetas disponibles.</p>
@@ -630,25 +671,7 @@ const DataStore = () => {
           </div>
         </div>
       )}
-      <div className="cart">
-        <h3>Carrito de compras</h3>
-        {cartItems.length > 0 ? (
-          <ul>
-            {cartItems.map((item, index) => (
-              <StoreCards
-                key={index}
-                title={item.title}
-                description={item.description}
-                price={item.price}
-                image={item.image}
-                mandarCarrito={removeFromCart(item)}
-              />
-            ))}
-          </ul>
-        ) : (
-          <p>El carrito está vacío.</p>
-        )}
-      </div>
+      
     </div>
   );
 };
